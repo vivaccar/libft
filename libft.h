c03e915013f23e6b89d720e6e2906fc1fb0e9d5f
Vinicius Vaccari Hehn <vivaccar@c1r4s4.42porto.com>
2023-10-20 20:19:42 +0100

    day04

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vivaccar <vivaccar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 12:48:15 by vivaccar          #+#    #+#             */
-/*   Updated: 2023/10/19 18:46:10 by vivaccar         ###   ########.fr       */
+/*   Updated: 2023/10/20 20:17:58 by vivaccar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
 int		ft_memcmp(const void *str1, const void *str2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+void	*ft_calloc(size_t n_elem, size_t size);
+char	*ft_strdup(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
