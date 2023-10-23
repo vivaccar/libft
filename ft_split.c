6bc8a4f3acf7ac84b5092c38eb1b995b91eada00
vivaccar <viniciusvaccarih@hotmail.com>
2023-10-23 13:35:52 +0100

    split and strtrim

--- ft_split.c
+++ ft_split.c
@@ -1,35 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vinivaccari <vinivaccari@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/18 15:52:55 by vivaccar          #+#    #+#             */
-/*   Updated: 2023/10/23 12:23:59 by vinivaccari      ###   ########.fr       */
+/*   Created: 2023/10/23 12:53:05 by vinivaccari       #+#    #+#             */
+/*   Updated: 2023/10/23 13:35:14 by vinivaccari      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-char	*ft_strchr(const char *s, int c)
+int count_words(const char *s)
 {
-	while (*s)
-	{
-		if (*s == (char) c)
-			return ((char *)s);
-		s++;
-	}
-	if ((char) c == '\0')
-		return ((char *)s);
-	return (0);
+    
 }
-/*#include <stdio.h>
 
-int	main(int argc, char *argv[])
+char    **ft_split(char const *s, char c)
 {
-	if (argc == 3)
-	{
-		printf("%s", ft_strchr(argv[1], argv[2][0]));
-	}
-}*/
+
+}
